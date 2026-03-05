44c8f2770ab9f681f96bc75d67ddb2d9a672052b
yogskr <yogakrisanta@gmail.com>
2026-03-06 01:16:30 +0700

    Remove download link for resume from Resume component

--- src/pages/resume/Resume.jsx
+++ src/pages/resume/Resume.jsx
@@ -3,9 +3,6 @@ export function Resume() {
     <section className="resume">
       <h2>Resume</h2>
       <p>Download my resume to learn more about my experience and skills.</p>
-      <a href="/resume.pdf" download>
-        Download Resume
-      </a>
     </section>
   );
 }
