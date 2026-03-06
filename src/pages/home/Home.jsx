af234da5964d849900cb87cd396d0e0840fa4ec1
yogskr <yogakrisanta@gmail.com>
2026-03-06 23:39:33 +0700

    Refactor Home component to implement hero section and integrate RotatingText

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,20 @@
-import { homeData } from "../../data";
 import "./Home.css";
+import { heroData } from "../../data";
+import { NavBar } from "../../components/navigation/NavBar";
+import RotatingText from "../../components/rotating-text/RotatingText";
 
 export function Home() {
   return (
-    <section className="home">
-      <h2>{homeData.title}</h2>
-      <p>{homeData.description}</p>
-    </section>
+    <div className="home">
+      <section className="hero">
+        <img className="heroImage" src={heroData.image} alt={heroData.title} />
+        <h1>{heroData.title}</h1>
+        <p>
+          {heroData.description}
+          <RotatingText />
+        </p>
+      </section>
+      <NavBar />
+    </div>
   );
 }
