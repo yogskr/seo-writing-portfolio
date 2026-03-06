ea288069f1cb44b10e940ec995adbcfa5a97ad29
yogskr <yogakrisanta@gmail.com>
2026-03-06 23:39:18 +0700

    Update phrases in RotatingText component to include Technical Writer

--- src/components/rotating-text/RotatingText.jsx
+++ src/components/rotating-text/RotatingText.jsx
@@ -7,11 +7,7 @@ export default function RotatingText() {
   const [isDeleting, setIsDeleting] = useState(false);
 
   useEffect(() => {
-    const phrases = [
-      "SEO Content Writer",
-      "News Writer",
-      "Front-End Web Developer",
-    ];
+    const phrases = ["SEO Content Writer", "News Writer", "Technical Writer"];
     const currentPhrase = phrases[wordIndex];
     let typingSpeed = isDeleting ? 50 : 100;
 
