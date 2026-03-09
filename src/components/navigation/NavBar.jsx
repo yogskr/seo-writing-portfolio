a9f197f1a74f1051a2dd41c585faaf13c596edb9
yogskr <yogakrisanta@gmail.com>
2026-03-09 15:36:54 +0700

    Update NavBar component styles and enhance link activation logic

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -2,13 +2,16 @@ import "./NavBar.css";
 import { navData } from "../../data";
 import { Link } from "react-router-dom";
 
-export function NavBar({ showNavBar }) {
+export function NavBar({ showNavBar, currentPath }) {
   return (
     <nav className={showNavBar ? "navContainer with-bg" : "navContainer"}>
       <ul className="navbar">
         {navData.links.map((link) => (
           <li key={link.name}>
-            <Link className="navlink" to={link.url}>
+            <Link
+              className={`navlink ${currentPath === link.url ? "active" : ""}`}
+              to={link.url}
+            >
               <i className={link.icon}></i>
               {link.name}
             </Link>
