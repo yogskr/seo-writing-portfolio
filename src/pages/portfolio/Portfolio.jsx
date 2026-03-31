d095a400be7a577d0ec0b8ca8d55028f19870f31
yogskr <yogakrisanta@gmail.com>
2026-03-31 16:23:37 +0700

    Modify Portfolio section

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -1,11 +1,40 @@
+import { portfolioData } from "../../data";
 import "./Portfolio.css";
+import {
+  bitdegreeCard,
+  hostingerCard,
+  wordpressCard,
+} from "../../components/cards/Cards";
 
 export function Portfolio() {
   return (
-    <section className="portfolio">
-      <h2>Portfolio</h2>
-      <p>Check out some of my recent writing projects and collaborations.</p>
-      {/* Add portfolio items here */}
-    </section>
+    <>
+      <section className="portfolio-header">
+        <h2>{portfolioData.header.title}</h2>
+      </section>
+      <section className="portfolio-content">
+        <hr className="line-break"></hr>
+        <div className="bitdegree">
+          <p className="description">
+            Currated articles and news from {portfolioData.companies[0]}
+          </p>
+          {bitdegreeCard()}
+        </div>
+        <hr className="line-break"></hr>
+        <div className="hostinger">
+          <p className="description">
+            Currated articles from {portfolioData.companies[1]}
+          </p>
+          {hostingerCard()}
+        </div>
+        <hr className="line-break"></hr>
+        <div className="wordpress">
+          <p className="description">
+            Currated articles from {portfolioData.companies[2]}
+          </p>
+          {wordpressCard()}
+        </div>
+      </section>
+    </>
   );
 }
