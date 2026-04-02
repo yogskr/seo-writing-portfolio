83cacdaa0f1d7d46a2c47e84ae29242bd25c054a
yogskr <yogakrisanta@gmail.com>
2026-04-02 15:07:48 +0700

    Add logo images to portfolio page

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -13,22 +13,37 @@ export function Portfolio() {
         <h2>{portfolioData.header.title}</h2>
       </section>
       <section className="portfolio-content">
-        <hr className="line-break"></hr>
+        <hr className="line-break" />
         <div className="bitdegree">
+          <img
+            src={portfolioData.logos.bitdegree}
+            alt={portfolioData.companies[0]}
+            className="logo bitdegree-logo"
+          />
           <p className="description">
             Currated articles and news from {portfolioData.companies[0]}
           </p>
           {bitdegreeCard()}
         </div>
-        <hr className="line-break"></hr>
+        <hr className="line-break" />
         <div className="hostinger">
+          <img
+            src={portfolioData.logos.hostinger}
+            alt={portfolioData.companies[1]}
+            className="logo hostinger-logo"
+          />
           <p className="description">
             Currated articles from {portfolioData.companies[1]}
           </p>
           {hostingerCard()}
         </div>
-        <hr className="line-break"></hr>
+        <hr className="line-break" />
         <div className="wordpress">
+          <img
+            src={portfolioData.logos.wordpress}
+            alt={portfolioData.companies[2]}
+            className="logo wordpress-logo"
+          />
           <p className="description">
             Currated articles from {portfolioData.companies[2]}
           </p>
