b219d5518f2ba2d33718c7d117e7c4f20de53727
yogskr <yogakrisanta@gmail.com>
2026-04-02 15:03:54 +0700

    Edit phrases

--- src/components/rotating-text/RotatingText.jsx
+++ src/components/rotating-text/RotatingText.jsx
@@ -7,7 +7,7 @@ export default function RotatingText() {
   const [isDeleting, setIsDeleting] = useState(false);
 
   useEffect(() => {
-    const phrases = ["SEO Content Writer", "News Writer", "Technical Writer"];
+    const phrases = ["SEO Content Writer", "News Writer", "Content Editor"];
     const currentPhrase = phrases[wordIndex];
     let typingSpeed = isDeleting ? 50 : 100;
 
