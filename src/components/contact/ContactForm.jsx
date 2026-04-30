7f28188ab0365e4896c51ae9ff5dc339cefc4cb9
yogskr <yogakrisanta@gmail.com>
2026-04-30 18:44:48 +0700

    Update ContactForm.jsx

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import emailjs from "@emailjs/browser";
 import { contactData } from "../../data";
-import "./ContactForm.css";
 
 export function ContactForm({ emailjsConfig }) {
   const [formData, setFormData] = useState({
