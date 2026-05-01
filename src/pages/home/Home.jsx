f598eb326d8518fbf3e0f6d0fb6d5d64b8a28f91
yogskr <yogakrisanta@gmail.com>
2026-05-01 07:01:58 +0700

    Modify and update styling

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,11 @@
-import "./Home.css";
 import { heroData, homeData } from "../../data";
 import { NavBar } from "../../components/navigation/NavBar";
 import RotatingText from "../../components/rotating-text/RotatingText";
+import "./Home.css";
 
 export function Home() {
   return (
     <div className="home">
-      <h1 className="hero-title">
-        {heroData.description}
-        <RotatingText />
-      </h1>
       <section className="hero-container">
         <div className="hero-content">
           <img
@@ -17,6 +13,10 @@ export function Home() {
             src={heroData.image}
             alt={heroData.title}
           />
+          <h1 className="hero-title">
+            {heroData.description}
+            <RotatingText />
+          </h1>
           <div className="hero-description">
             <p className="hero-name">{heroData.title}</p>
             <p className="hero-text">{homeData.description}</p>
