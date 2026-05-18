61658de07588fce14b6de7c8666895817adced9d
yogskr <yogakrisanta@gmail.com>
2026-05-19 01:59:22 +0700

    Add Tailwind styling

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
 import { ContactForm } from "../../components/contact/ContactForm";
 import { ContactInfo } from "../../components/contact/ContactInfo";
-import { useIsScrolled } from "../../hooks/stickyHeader";
 import { contactData } from "../../data";
+import useIsScrolled from "../../hooks/useIsScrolled";
 import emailjs from "@emailjs/browser";
 
 export function Contact() {
@@ -20,28 +20,46 @@ export function Contact() {
     emailjs.init(emailjsConfig.publicKey);
   }, [emailjsConfig.publicKey]);
 
-  const isScrolled = useIsScrolled();
+  const stickyHeader = useIsScrolled();
 
   return (
-    <div className="contact-container">
-      <section
-        className={isScrolled ? "contact-header scrolled" : "contact-header"}
-      >
-        <h2 className="contact-title">{contactData.header.title}</h2>
-      </section>
-      <section className="contact-content">
-        <hr className="line-break" />
-        <p className="contact-description">{contactData.header.description}</p>
-        <div className="contact-info-section">
-          <ContactInfo contactData={contactData} />
-        </div>
-
-        <hr className="line-break" />
-        <div className="contact-form-section">
-          <h3 className="section-title">Let's Talk About Projects</h3>
+    <>
+      <div className="bg-snow-storm-nord6 p-4">
+        <section
+          className={`${stickyHeader ? "fixed left-0 right-0 rounded-none shadow-none" : "sticky shadow-small"} top-0 flex flex-wrap items-center justify-between gap-4 bg-pollar-night-nord0 text-snow-storm-nord5 p-4 rounded-lg z-50`}
+        >
+          <h2 className="text-lg font-bold font-cascadia-code text-snow-storm-nord6">
+            {contactData.header.title}
+          </h2>
+          <p className="text-sm text-frost-nord7 tracking-tight font-jetbrains-mono">
+            {contactData.header.description}
+          </p>
+        </section>
+      </div>
+      <div className="grid grid-cols-12 auto-rows-auto gap-y-4 gap-x-3 p-4">
+        <ContactInfo contactData={contactData} />
+        <div className="col-span-12 border-2 border-solid border-pollar-night-nord3 p-4 rounded-lg flex flex-col gap-4 shadow-small bg-snow-storm-nord5">
+          <h3 className="text-sm lowercase font-cascadia-code text-pollar-night-nord0">
+            {contactData.formFields.title}
+          </h3>
           <ContactForm emailjsConfig={emailjsConfig} />
         </div>
-      </section>
-    </div>
+        <div className="col-span-12 flex flex-col p-4 gap-4 border-2 border-solid border-pollar-night-nord3 rounded-lg shadow-small bg-frost-nord8">
+          <h3 className="lowercase text-sm font-cascadia-code">
+            {contactData.availability.title}
+          </h3>
+          <ul className="lowercase flex flex-wrap gap-2">
+            {contactData.availability.description.map((job) => (
+              <li
+                key={job.id}
+                className="border-2 border-solid border-pollar-night-nord0 font-jetbrains-mono px-2 py-1 rounded-md"
+              >
+                {job.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
