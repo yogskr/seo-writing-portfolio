5910824c3a7875944d31ff09707ede80a86a1ff3
yogskr <yogakrisanta@gmail.com>
2026-05-28 04:47:46 +0700

    Modify styling

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -7,23 +7,25 @@ import {
 
 function bitdegreeCard() {
   return (
-    <section className="flex flex-col gap-4">
+    <section className="flex flex-col gap-4 md:gap-6">
       {bitdegreeArticles.map((article) => (
         <div
           key={article.id}
-          className="flex flex-col gap-3 border-2 border-solid border-pollar-night-nord3 p-4 rounded-lg shadow-small bg-snow-storm-nord5"
+          className="flex flex-col gap-3 md:gap-5 border-2 border-solid border-pollar-night-nord3 p-4 md:px-8 md:py-6 rounded-lg shadow-small bg-snow-storm-nord5"
         >
-          <h3 className="bg-aurora-nord14 rounded-lg px-2 py-1 font-semibold font-cascadia-code text-pollar-night-nord0">
+          <h3 className="bg-aurora-nord14 rounded-lg px-2 py-1 md:px-3 md:py-2 font-semibold font-cascadia-code text-pollar-night-nord0 md:text-xl">
             {article.title}
           </h3>
-          <p className="px-0.5 font-jetbrains-mono">{article.description}</p>
+          <p className="px-0.5 font-jetbrains-mono md:text-lg">
+            {article.description}
+          </p>
           <hr className="border-dashed" />
-          <button className="lowercase self-end border border-solid border-pollar-night-nord3 bg-aurora-nord13 px-2 py-1 rounded-md">
+          <button className="lowercase self-end border border-solid border-pollar-night-nord3 bg-aurora-nord13 px-2 py-1 md:px-3 md:py-2 rounded-md md:text-lg">
             <a
               href={article.url}
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center gap-1.5 text-pollar-night-nord1 font-semibold font-cascadia-code"
+              className="flex items-center gap-1.5 md:gap-3 text-pollar-night-nord1 font-semibold font-cascadia-code"
             >
               {workData.readMore.title}
               <i className={workData.readMore.icon}></i>
@@ -37,23 +39,25 @@ function bitdegreeCard() {
 
 function hostingerCard() {
   return (
-    <section className="flex flex-col gap-4">
+    <section className="flex flex-col gap-4 md:gap-6">
       {hostingerArticles.map((article) => (
         <div
           key={article.id}
-          className="flex flex-col gap-3 border-2 border-solid border-pollar-night-nord3 p-4 rounded-lg shadow-small bg-snow-storm-nord5"
+          className="flex flex-col gap-3 md:gap-5 border-2 border-solid border-pollar-night-nord3 p-4 md:px-8 md:py-6 rounded-lg shadow-small bg-snow-storm-nord5"
         >
-          <h3 className="bg-aurora-nord14 rounded-lg px-2 py-1 font-semibold font-cascadia-code text-pollar-night-nord0">
+          <h3 className="bg-aurora-nord14 rounded-lg px-2 py-1 md:px-3 md:py-2 font-semibold font-cascadia-code text-pollar-night-nord0 md:text-xl">
             {article.title}
           </h3>
-          <p className="px-0.5 font-jetbrains-mono">{article.description}</p>
+          <p className="px-0.5 font-jetbrains-mono md:text-lg">
+            {article.description}
+          </p>
           <hr className="border-dashed" />
-          <button className="lowercase self-end border border-solid border-pollar-night-nord3 bg-aurora-nord13 px-2 py-1 rounded-md">
+          <button className="lowercase self-end border border-solid border-pollar-night-nord3 bg-aurora-nord13 px-2 py-1 md:px-3 md:py-2 rounded-md md:text-lg">
             <a
               href={article.url}
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center gap-1.5 text-pollar-night-nord1 font-semibold font-cascadia-code"
+              className="flex items-center gap-1.5 md:gap-3 text-pollar-night-nord1 font-semibold font-cascadia-code"
             >
               {workData.readMore.title}
               <i className={workData.readMore.icon}></i>
@@ -67,23 +71,25 @@ function hostingerCard() {
 
 function wordpressCard() {
   return (
-    <section className="flex flex-col gap-4">
+    <section className="flex flex-col gap-4 md:gap-6">
       {wordpressArticles.map((article) => (
         <div
           key={article.id}
-          className="flex flex-col gap-3 border-2 border-solid border-pollar-night-nord3 p-4 rounded-lg shadow-small bg-snow-storm-nord5"
+          className="flex flex-col gap-3 md:gap-5 border-2 border-solid border-pollar-night-nord3 p-4 md:px-8 md:py-6 rounded-lg shadow-small bg-snow-storm-nord5"
         >
-          <h3 className="bg-aurora-nord14 rounded-lg px-2 py-1 font-semibold font-cascadia-code text-pollar-night-nord0">
+          <h3 className="bg-aurora-nord14 rounded-lg px-2 py-1 md:px-3 md:py-2 font-semibold font-cascadia-code text-pollar-night-nord0 md:text-xl">
             {article.title}
           </h3>
-          <p className="px-0.5 font-jetbrains-mono">{article.description}</p>
+          <p className="px-0.5 font-jetbrains-mono md:text-lg">
+            {article.description}
+          </p>
           <hr className="border-dashed" />
-          <button className="lowercase self-end border border-solid border-pollar-night-nord3 bg-aurora-nord13 px-2 py-1 rounded-md">
+          <button className="lowercase self-end border border-solid border-pollar-night-nord3 bg-aurora-nord13 px-2 py-1 md:px-3 md:py-2 rounded-md md:text-lg">
             <a
               href={article.url}
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center gap-1.5 text-pollar-night-nord1 font-semibold font-cascadia-code"
+              className="flex items-center gap-1.5 md:gap-3 text-pollar-night-nord1 font-semibold font-cascadia-code"
             >
               {workData.readMore.title}
               <i className={workData.readMore.icon}></i>
