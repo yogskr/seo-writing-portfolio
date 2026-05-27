b90d036cea0b1ab5bf4464c9287ba627249321e0
yogskr <yogakrisanta@gmail.com>
2026-05-28 04:46:17 +0700

    Modify styling

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,21 +4,21 @@ import RotatingText from "../../components/rotating-text/RotatingText";
 
 export function Home() {
   return (
-    <div className="grid grid-cols-10 auto-rows-auto gap-4 md:gap-6 p-4 md:p-6 h-screen">
+    <div className="grid grid-cols-10 auto-rows-auto gap-4 md:gap-6 p-4 md:p-8 min-h-svh">
       {/* Hero Section */}
-      <header className="col-span-10 md:col-span-5 row-span-5 md:row-span-10 flex flex-col justify-between p-4 md:px-8 md:py-10 bg-pollar-night-nord0 border-2 border-solid border-pollar-night-nord3 rounded-lg shadow-small">
+      <header className="col-span-10 md:col-span-5 row-span-5 md:row-span-10 flex flex-col justify-between p-4 md:p-8 bg-pollar-night-nord0 border-2 border-solid border-pollar-night-nord3 rounded-lg shadow-small">
         <figure className="w-full h-fit rounded-lg col-start-2 flex justify-end">
           <img
             src={heroData.image}
             alt={heroData.title}
-            className="w-32 md:w-56 h-fit rounded-lg border-2 border-solid border-pollar-night-nord1"
+            className="w-28 md:w-52 h-fit rounded-lg border-2 border-solid border-pollar-night-nord1"
           />
         </figure>
         <div className="flex flex-col gap-2 md:gap-6">
           <h1 className="text-snow-storm-nord4 text-sm md:text-base font-cascadia-code">
             {heroData.title}
           </h1>
-          <div className="font-jetbrains-mono md:flex md:flex-col md:gap-4">
+          <div className="font-jetbrains-mono md:flex md:flex-col md:gap-4 mb-4 md:mb-0">
             <p className="text-snow-storm-nord5 font-bold text-lg md:text-2xl">
               {heroData.author}
             </p>
@@ -27,9 +27,9 @@ export function Home() {
             </p>
           </div>
         </div>
-        <div className="flex items-center gap-1.5 bg-aurora-nord12 w-fit px-2 py-1 md:px-4 md:py-2 rounded-md text-pollar-night-nord0 hover:bg-aurora-nord14 transition-colors">
+        <div className="flex items-center justify-center gap-1.5 md:gap-3 bg-aurora-nord12 w-fit px-2 py-1 md:px-4 md:py-2 rounded-md text-pollar-night-nord0 hover:bg-aurora-nord14 transition-colors">
           <i
-            className={`${heroData.downloadResume.icon} text-pollar-nord1`}
+            className={`${heroData.downloadResume.icon} text-pollar-nord1 md:text-lg`}
           ></i>
           <a
             href={heroData.downloadResume.link}
@@ -64,7 +64,7 @@ export function Home() {
           {homeData.niches.list.map((niche, index) => (
             <li
               key={index}
-              className="text-sm text-pollar-night-nord0 font-bold p-2 border-2 border-solid border-pollar-night-nord1 rounded-lg font-jetbrains-mono"
+              className="text-sm md:text-base text-pollar-night-nord0 font-bold p-2 border-2 border-solid border-pollar-night-nord1 rounded-lg font-jetbrains-mono"
             >
               {niche}
             </li>
