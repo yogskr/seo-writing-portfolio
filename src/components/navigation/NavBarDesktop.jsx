bd3f0a703cb0da72cd5c497c6d68304c2fd45427
yogskr <yogakrisanta@gmail.com>
2026-06-30 17:02:22 +0700

    Modify styling for desktop navigation bar

--- src/components/navigation/NavBarDesktop.jsx
+++ src/components/navigation/NavBarDesktop.jsx
@@ -7,12 +7,16 @@ export function NavBarDesktop({ currentPath }) {
 
   return (
     <nav className="hidden lg:flex justify-between items-center px-8 py-4 col-span-10 bg-pollar-night-nord0 text-snow-storm-nord5 rounded-lg shadow-small text-xl font-jetbrains-mono">
-      <div className="flex space-x-4">
+      <div className="flex space-x-6">
         {navData.links.map((link) => (
           <Link
             key={link.id}
             to={link.url}
-            className={path === link.url ? "active" : ""}
+            className={
+              path === link.url
+                ? "text-aurora-nord14 font-bold"
+                : "cursor-pointer hover:text-aurora-nord13"
+            }
           >
             {link.name}
           </Link>
